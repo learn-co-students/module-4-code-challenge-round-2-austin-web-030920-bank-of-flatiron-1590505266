b50dfef7e9287a7f01e451aa25839a68d17263b2
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-05-26 12:00:37 -0500

    submission for deadline

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,72 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+URL = `http://localhost:6001/transactions`
 class AccountContainer extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      transactions: [],
+      // dateValue: '',
+      // descValue: '',
+      // catValue: '',
+      // amountValue: ''
+    }
+  }
+
+  componentDidMount() {
+    this.getTransactions()
+  }
+
+  // handleDate(e) {
+  //   this.setState({dateValue: e.target.value})
+  // }
+
+  // handleDesc(e) {
+  //   this.setState({descValue: e.target.value})
+  // }
+
+  // handleCat(e) {
+  //   this.setState({catValue: e.target.value})
+  // }
+
+  // handleAmount(e) {
+  //   this.setState({amountValue: e.target.value})
+  // }
+
+  // addTransaction(e) {
+  //   let newTran = {
+  //     "id": `${this.state.transactions.length+1}`,
+  //     "date": `${this.handleDate()}`,
+  //     "description": `${this.handleDesc()}`,
+  //     "category": `${this.handleCat()}`,
+  //     "amount": `${this.handleAmount()}`
+  //   };
+  //   this.setState({transactions: this.state.transactions.unshift(newTran)});
+
+  //   fetch(URL, {
+  //     method: "POST"
+  //   })
+  // }
+
+  searchTransactions = (e) => {
+    console.log("Searched!")
+    this.setState({transactions: this.state.transactions.filter(t => t.description === e.target.input.value)})
+  }
+  
+  getTransactions = () => {
+    fetch(URL)
+      .then(rsp => rsp.json())
+      .then(json => this.setState({transactions: json}))
+  }
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search handleSearch={this.searchTransactions}/>
+        <AddTransactionForm handleSubmit={this.addTransaction} handleDate={this.handleDate} handleAmount={this.handleAmount} handleCat={this.handleCat} handleDesc={this.handleDesc}/>
+        <TransactionsList transactions={this.state.transactions} />
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,22 +1,56 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      transactions: [],
+      dateValue: '',
+      descValue: '',
+      catValue: '',
+      amountValue: ''
+    }
+  }
+
+
+  
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input 
+            type="date" 
+            name="date" 
+            // value={this.state.dateValue} 
+            // onChange={this.props.handleDate()}
+            />
+            <input 
+            type="text" 
+            name="description" 
+            placeholder="Description" 
+            // value={this.props.descValue} 
+            // onChange={this.props.handleDesc()}
+            />
+            <input 
+            type="text" 
+            name="category" 
+            placeholder="Category" 
+            // value={this.state.catValue} 
+            // onChange={this.props.handleCat()} 
+            />
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              // value={this.state.amountValue}
+              // onChange={this.props.handleAmount()}
             />
           </div>
-          <button className="ui button" type="submit">
+          <button className="ui button" type="submit" 
+          // onSubmit={() => this.props.handleSubmit()}
+          >
             Add Transaction
           </button>
         </form>
