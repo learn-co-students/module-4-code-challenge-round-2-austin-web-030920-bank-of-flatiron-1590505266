072c7b03d0e90c282efeed2368801798357fdaac
jnuzzi4 <jnuzzi47@gmail.com>
2020-05-26 12:59:23 -0500

    submit to deadline

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,65 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const URL = "http://localhost:6001/transactions"
+
 class AccountContainer extends Component {
+  constructor(){
+    super()
+    this.state = {
+      allTrans: null,
+      newTrans: null,
+      filterTrans: null
+    }
+  }
+
+  componentDidMount = () => {
+    fetch(URL).then(r => r.json()).then(allTrans => this.setState({ allTrans }));
+  }
+
+  handleSubmit = (newT) => {
+    this.setState({newTrans: newT})
+    this.updateDB(newT)
+    this.newFetch()
+  }
+
+  updateDB = (newT) => {
+    fetch(URL, {
+      method: 'POST',
+      headers:
+      {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(newT )
+    }).then(r => r.json()).then(json => console.log(json))
+  }
+
+  newFetch = () => {
+    fetch(URL).then(r => r.json()).then(allTrans => this.setState({ allTrans }));
+  }
+
+  handleSearch = (e) => {
+    e.persist();
+    e.preventDefault();
+    let filteredTrans = [];
+    this.state.allTrans.filter(tran => {
+      if (tran.description === e.target.value) {
+          filteredTrans.push(tran)
+          this.setState({allTrans: filteredTrans})
+          // if (!this.state.filterTrans) {
+            
+          // }
+      }
+    })
+  }
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search submitSearch={this.handleSearch}/>
+        <AddTransactionForm submitClick={this.handleSubmit}/>
+        <TransactionsList transactions={this.state.allTrans}/>
       </div>
     );
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,17 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-const TransactionsList = () => {
+
+const TransactionsList = (props) => {
+
+  function mapTransactions() {
+    if (props.transactions !== null) {
+      return props.transactions.map(trans => {
+        return <Transaction key={Math.random()} transaction={trans}/>
+      })
+    }
+  }
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +29,7 @@ const TransactionsList = () => {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render Transactions here */}
+        {mapTransactions()}
       </tbody>
     </table>
   );
