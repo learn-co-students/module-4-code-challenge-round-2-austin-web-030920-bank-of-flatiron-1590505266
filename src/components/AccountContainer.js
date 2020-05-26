b882f2a7415635f11417c0f17c6c489838ea34e5
J-Fisk87 <Jeff.Fisk512@gmail.com>
2020-05-26 10:22:54 -0500

    first deliverable

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,26 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const URL = "http://localhost:6001/transactions";
+
 class AccountContainer extends Component {
+  constructor(){
+    super();
+    this.state = {
+      transactions: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(URL).then(r => r.json()).then(json => this.setState({transactions: json}))
+  }
+
   render() {
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList trans={this.state.transactions} />
       </div>
     );
   }
